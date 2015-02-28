3126890d71db011b5dbbb730d047f803e1a643d4
Phil London <philip.n.london@gmail.com>
2015-02-28 11:14:52 -0500

    add foggell (time theme, get it)

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express();
 var cool = require('cool-ascii-faces');
 
+var video = "<iframe style='width: 100%; height: 100%;' src='https://www.youtube.com/embed/eRZoLV7JV7s'></iframe>";
+
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  response.send(video);
 });
 
 app.listen(app.get('port'), function() {
